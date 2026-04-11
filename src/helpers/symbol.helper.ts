ea4f656f89f36f21c07759ede48df7d8d610c100
ivan.kosenko <foxartorg@gmail.com>
2026-04-11 06:25:03 +0300

    фикс

--- src/helpers/symbol.helper.ts
+++ src/helpers/symbol.helper.ts
@@ -1,8 +1,10 @@
 export type SymbolArrowKeyType = 'LEFT' | 'UP' | 'RIGHT' | 'DOWN';
 export type SymbolStatusKeyType = 'SUCCESS' | 'ERROR' | 'WARNING';
+export type SymbolCommonKeyType = 'SEPARATOR';
 
 type SymbolArrowMapType = Readonly<Record<SymbolArrowKeyType, string>>;
 type SymbolStatusMapType = Readonly<Record<SymbolStatusKeyType, string>>;
+type SymbolCommonMapType = Readonly<Record<SymbolCommonKeyType, string>>;
 
 const ARROW = {
   LEFT: '\u2190', // ←
@@ -17,17 +19,24 @@ const STATUS = {
   WARNING: '\u26A0', // ⚠
 } as const satisfies SymbolStatusMapType;
 
+const COMMON = {
+  SEPARATOR: '\u2503', // ┃
+} as const satisfies SymbolCommonMapType;
+
 export type SymbolArrowValueType = (typeof ARROW)[keyof typeof ARROW];
 export type SymbolStatusValueType = (typeof STATUS)[keyof typeof STATUS];
+export type SymbolCommonValueType = (typeof COMMON)[keyof typeof COMMON];
 
 export type SymbolArrowType = typeof ARROW;
 export type SymbolStatusType = typeof STATUS;
+export type SymbolCommonType = typeof COMMON;
 
 class SymbolSingleton {
   private static self: SymbolSingleton;
 
   public readonly arrow = ARROW;
   public readonly status = STATUS;
+  public readonly common = COMMON;
 
   public static getInstance(): SymbolSingleton {
     if (!SymbolSingleton.self) {
