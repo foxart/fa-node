837f38c51876189a64bee56c0dab10e9915baf97
ivan.kosenko <foxartorg@gmail.com>
2026-04-11 04:51:13 +0300

    fix

--- src/helpers/system.helper.ts
+++ src/helpers/system.helper.ts
@@ -26,15 +26,15 @@ class SystemSingleton {
     return parseFloat((diff[0] * 1e3 + diff[1] / 1e6).toFixed(3));
   }
 
-  public millisecondsToHhMmSs(milliseconds: number): string {
-    const hours = Math.floor(milliseconds / 3600000);
-    const minutes = Math.floor((milliseconds % 3600000) / 60000);
-    const seconds = Math.floor((milliseconds % 60000) / 1000);
-    const formattedHours = String(hours).padStart(2, '0');
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(seconds).padStart(2, '0');
-    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
-  }
+  // public millisecondsToHhMmSs(milliseconds: number): string {
+  //   const hours = Math.floor(milliseconds / 3600000);
+  //   const minutes = Math.floor((milliseconds % 3600000) / 60000);
+  //   const seconds = Math.floor((milliseconds % 60000) / 1000);
+  //   const formattedHours = String(hours).padStart(2, '0');
+  //   const formattedMinutes = String(minutes).padStart(2, '0');
+  //   const formattedSeconds = String(seconds).padStart(2, '0');
+  //   return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  // }
 
   public sleep(time: string): Promise<void> {
     return new Promise((resolve) => {
