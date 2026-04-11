8ae9b151e7803cb4692dd40b6a831dfed7a31df0
ivan.kosenko <foxartorg@gmail.com>
2026-04-11 04:51:05 +0300

    fix

---
+++ src/helpers/color.helper.ts
@@ -0,0 +1,52 @@
+class ColorSingleton {
+  private static self: ColorSingleton;
+
+  public readonly effect = {
+    RESET: '\x1b[0m',
+    BOLD: '\x1b[1m',
+    DIM: '\x1b[2m',
+    UNDERLINE: '\x1b[4m',
+    BLINK: '\x1b[5m',
+    REVERSE: '\x1b[7m',
+    HIDDEN: '\x1b[8m',
+  };
+
+  public readonly foreground = {
+    BLACK: '\x1b[30m',
+    RED: '\x1b[31m',
+    GREEN: '\x1b[32m',
+    YELLOW: '\x1b[33m',
+    BLUE: '\x1b[34m',
+    MAGENTA: '\x1b[35m',
+    CYAN: '\x1b[36m',
+    WHITE: '\x1b[37m',
+    GRAY: '\x1b[90m',
+  };
+
+  public readonly background = {
+    BLACK: '\x1b[40m',
+    RED: '\x1b[41m',
+    GREEN: '\x1b[42m',
+    YELLOW: '\x1b[43m',
+    BLUE: '\x1b[44m',
+    MAGENTA: '\x1b[45m',
+    CYAN: '\x1b[46m',
+    WHITE: '\x1b[47m',
+    GRAY: '\x1b[100m',
+  };
+
+  public static getInstance(): ColorSingleton {
+    if (!ColorSingleton.self) {
+      ColorSingleton.self = new ColorSingleton();
+    }
+    return ColorSingleton.self;
+  }
+
+  public wrapData(data: string, colors: string[]): string {
+    return colors.reduce((acc, color) => {
+      return `${color}${acc}${this.effect.RESET}`;
+    }, data);
+  }
+}
+
+export const ColorHelper = ColorSingleton.getInstance();
---
+++ src/helpers/symbol.helper.ts
@@ -0,0 +1,25 @@
+class SymbolSingleton {
+  private static self: SymbolSingleton;
+
+  public readonly arrow = {
+    LEFT: '\u2190', // ←
+    UP: '\u2191', // ↑
+    RIGHT: '\u2192', // →
+    DOWN: '\u2193', // ↓
+  };
+
+  public readonly status = {
+    SUCCESS: '\u2714', // ✔
+    ERROR: '\u2716', // ✖
+    WARNING: '\u26A0', // ⚠
+  };
+
+  public static getInstance(): SymbolSingleton {
+    if (!SymbolSingleton.self) {
+      SymbolSingleton.self = new SymbolSingleton();
+    }
+    return SymbolSingleton.self;
+  }
+}
+
+export const SymbolHelper = SymbolSingleton.getInstance();
