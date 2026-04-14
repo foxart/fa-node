852fed320c8e873c88f78e87c3a2e36557bb0a7a
ivan.kosenko <foxartorg@gmail.com>
2026-04-14 15:50:29 +0300

    ref(main): console: вынес настройки Express в bootstrap
    - Добавил поддержку NestExpressApplication для приложения
    - Перенес LoggerSystemService и бутстрап конфигурацию в main.ts
    - Обновил метод create с включением bufferLogs и useLogger

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+void (async function bootstrap(): Promise<void> {
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
+  const logger = app.get(LoggerSystemService);
+  app.useLogger(logger);
   await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+})();
